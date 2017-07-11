32c8dd23203686436a92e5ace6e8b1071421c511
TheInvoker <ryans.dsouza@mail.utoronto.ca>
2017-07-11 02:53:55 -0400

    update

--- server.js
+++ server.js
@@ -3,14 +3,6 @@ var app = express();
 
 app.use(express.static(__dirname + '/public'));
 
-app.get('/', function (req, res) {
-	var dest = 'A.html';
-	res.sendFile(dest, { root: __dirname });
-});
-
 var server = app.listen(3000, function () {
-	var host = server.address().address;
-	var port = server.address().port;
 
-	console.log('DSIL started at http://%s:%s', host, port);
 });
